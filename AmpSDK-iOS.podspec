0fc9b3c25cbb8c621d521a2d13f22fe146e45657
etre.tang <etre.tang@aihuishou.com>
2019-10-30 17:53:26 +0800

    v0.2.0

--- AmpSDK-iOS.podspec
+++ AmpSDK-iOS.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AmpSDK-iOS'
-  s.version          = '0.1.0'
+  s.version          = '0.2.0'
   s.summary          = 'AmpSDK-iOS is a small program based on the iOS.'
 
 # This description is used to generate tags and improve search results.
--- AmpSDK-iOS/AmpSDK_iOS.framework/Headers/AMPParams.h
+++ AmpSDK-iOS/AmpSDK_iOS.framework/Headers/AMPParams.h
@@ -16,10 +16,13 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, assign) NSInteger bundleId;
 
 /// 初始化URL
-@property (nonatomic, nullable, strong) NSString *initialUrl;
+@property (nonatomic, nullable, copy) NSString *initialUrl;
 
 /// 初始化传参
 @property (nonatomic, nullable, strong) NSDictionary *initialProperties;
+
+/// 业务版本号（必传，格式 x.x.x   例如 0.1.0）
+@property (nonatomic, copy) NSString *businessSdkVersion;
 
 /// 是否隐藏RN控制器导航栏
 @property (nonatomic, assign) BOOL hideNavigation;
